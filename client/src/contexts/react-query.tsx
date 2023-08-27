700772c522fb42b0cc2c5d87e90d3c51ff70a549
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-08-27 18:48:47 +0200

    feat: add context provider

--- client/src/contexts/react-query.tsx
+++ client/src/contexts/react-query.tsx
@@ -3,7 +3,7 @@
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
-export default function Providers({ children }: { children: React.ReactNode }) {
+export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
   const [queryClient] = useState(() => new QueryClient())
 
   return (
---
+++ client/src/contexts/theme.tsx
@@ -0,0 +1,33 @@
+'use client'
+
+import React, { createContext, useContext, useState } from 'react'
+
+type ThemeVariant = 'dark' | 'light' | 'system'
+
+type ThemeContext = {
+  theme: ThemeVariant
+  setTheme: React.Dispatch<React.SetStateAction<ThemeVariant>>
+}
+
+type ThemeContextProviderProps = {
+  children: React.ReactNode
+}
+
+export const ThemeContext = createContext<ThemeContext | null>(null)
+export default function ThemeContextProvider({ children }: ThemeContextProviderProps) {
+  const [theme, setTheme] = useState<ThemeVariant>('dark')
+
+  return (
+    <ThemeContext.Provider value={{ theme, setTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  )
+}
+
+export function useThemeContext() {
+  const context = useContext(ThemeContext)
+  if (!context) {
+    throw new Error('useThemeContext must be used within a ThemeContextProvider')
+  }
+  return context
+}
