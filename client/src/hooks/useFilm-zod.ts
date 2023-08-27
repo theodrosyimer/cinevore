c65af62b3678b626c21fee0303ebf1c07d827de8
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-08-27 18:46:10 +0200

    feat: add zod to runtime validation and type check custom hooks

---
+++ client/src/hooks/useFilm-zod.ts
@@ -0,0 +1,23 @@
+import { useQuery } from "@tanstack/react-query"
+import { z } from "zod"
+
+export const filmSchema = z.object({
+  id: z.number(),
+  title: z.string(),
+})
+
+export type Film = z.infer<typeof filmSchema>
+
+async function getFilm(id: number) {
+  const data = await fetch(`https://swapi.dev/api/films/${id}`).then((res) => res.json())
+  return filmSchema.parse(data.results)
+}
+
+export function useFilm(id: number) {
+  const filmData = useQuery({
+    queryKey: ['film', id],
+    queryFn: () => getFilm(id),
+  })
+  return filmData
+}
+
