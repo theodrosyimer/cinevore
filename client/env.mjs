74ec76ec985b016a4e2e52402b2d46c2bcc01af9
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-09-02 16:09:15 +0200

    feat(env.mjs): update env variables

--- client/env.mjs
+++ client/env.mjs
@@ -5,20 +5,27 @@ export const env = createEnv({
   server: {
     // This is optional because it's only used in development.
     // See https://next-auth.js.org/deployment.
-    // NEXTAUTH_URL: z.string().url().optional(),
-    // NEXTAUTH_SECRET: z.string().min(1),
+    NEXTAUTH_URL: z.string().url().optional(),
+    NEXTAUTH_SECRET: z.string().min(1),
     // GITHUB_CLIENT_ID: z.string().min(1),
     // GITHUB_CLIENT_SECRET: z.string().min(1),
     // GITHUB_ACCESS_TOKEN: z.string().min(1),
-    DATABASE_URL: z.string().min(1),
+    // DATABASE_URL: z.string().min(1),
+    DB_HOST: z.string().min(1),
+    DB_ADMIN: z.string().min(1),
+    DB_PASSWORD: z.string().min(1),
+    // DB_NAME: z.string().min(1),
+    DRIZZLE_DB_NAME: z.string().min(1),
+    DB_PORT: z.coerce.number().min(4).max(5),
+    JWT_SECRET: z.string().min(1),
   },
   client: {
     NEXT_PUBLIC_APP_URL: z.string().min(1),
     SERVER_URL: z.string().min(1)
   },
   runtimeEnv: {
-    // NEXTAUTH_URL: process.env.NEXTAUTH_URL,
-    // NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
+    NEXTAUTH_URL: process.env.NEXTAUTH_URL,
+    NEXTAUTH_SECRET: process.env.NEXTAUTH_SECRET,
     // GITHUB_CLIENT_ID: process.env.GITHUB_CLIENT_ID,
     // GITHUB_CLIENT_SECRET: process.env.GITHUB_CLIENT_SECRET,
     // GITHUB_ACCESS_TOKEN: process.env.GITHUB_ACCESS_TOKEN,
