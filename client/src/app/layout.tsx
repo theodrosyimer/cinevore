2ef3ec7d35a88ec9ce4372aa9b4fd3f3b4db7cd7
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-09-02 16:16:28 +0200

    refactor: refactor main layout

--- client/src/app/layout.tsx
+++ client/src/app/layout.tsx
@@ -1,40 +1,99 @@
 import '@/styles/globals.css'
-import { Inter } from 'next/font/google'
+// import { Inter } from 'next/font/google'
+import { Inter as FontSans } from "next/font/google"
+import localFont from "next/font/local"
 
 import ReactQueryProvider from '@/providers/react-query'
 // import ThemeContextProvider from '@/contexts/theme'
-import { Header } from '@/components/header'
-import { Footer } from '@/components/footer'
 import { ThemeProvider } from '@/components/theme-provider'
 import { TailwindIndicator } from '@/components/tailwind-indicator'
 import { cn } from '@/lib/utils'
+import { siteConfig } from '@/config/site'
 
-const inter = Inter({ subsets: ['latin'] })
+// const inter = Inter({ subsets: ['latin'] })
+
+
+const fontSans = FontSans({
+  subsets: ["latin"],
+  variable: "--font-sans",
+})
+
+// Font files can be colocated inside of `pages`
+const fontHeading = localFont({
+  src: "../assets/fonts/CalSans-SemiBold.woff2",
+  variable: "--font-heading",
+})
+
+interface RootLayoutProps {
+  children: React.ReactNode
+}
 
 export const metadata = {
-  title: 'Cinevore',
-  description: 'Cinevore your social network for film discovery.',
+  title: {
+    default: siteConfig.name,
+    template: `%s | ${siteConfig.name}`,
+  },
+  description: siteConfig.description,
+  keywords: [
+    "Cinema",
+    "Films",
+    "Movies",
+    "Social Network",
+    "Reviews",
+    "Next.js",
+    "React",
+    "Tailwind CSS",
+    "Server Components",
+    "Radix UI",
+  ],
+  authors: [
+    {
+      name: "theodros yimer",
+      url: "https://github.com/theodrosyimer",
+    },
+  ],
+  creator: "theodros yimer",
+  themeColor: [
+    { media: "(prefers-color-scheme: light)", color: "white" },
+    { media: "(prefers-color-scheme: dark)", color: "black" },
+  ],
+  openGraph: {
+    type: "website",
+    locale: "en_US",
+    url: siteConfig.url,
+    title: siteConfig.name,
+    description: siteConfig.description,
+    siteName: siteConfig.name,
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: siteConfig.name,
+    description: siteConfig.description,
+    images: [`${siteConfig.url}/og.jpg`],
+    creator: "@thyi",
+  },
+  icons: {
+    icon: "/favicon.ico",
+    shortcut: "/favicon-16x16.png",
+    apple: "/apple-touch-icon.png",
+  },
+  manifest: `${siteConfig.url}/site.webmanifest`,
 }
 
 export default function RootLayout({
-  children }: {
-    children: React.ReactNode
-  }) {
+  children }: RootLayoutProps) {
   return (
     <html lang="en">
       <body className={cn(
         "min-h-screen grid grid-rows-layout font-sans antialiased",
-        inter.className,
-        // fontSans.variable,
-        // fontHeading.variable
+        // inter.className,
+        fontSans.variable,
+        fontHeading.variable
       )}>
         <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
           <ReactQueryProvider>
-            {/* <Header /> */}
             {children}
-            {/* <Footer /> */}
             <TailwindIndicator />
-
           </ReactQueryProvider>
         </ThemeProvider>
       </body>
