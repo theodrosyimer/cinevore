7323f91895117f2cb479e129c46633d5319346f3
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-09-02 16:14:52 +0200

    feat: update site config for metadata

--- client/src/config/site.ts
+++ client/src/config/site.ts
@@ -3,11 +3,11 @@ import { SiteConfig } from "@/types"
 export const siteConfig: SiteConfig = {
   name: "Cinevore",
   description:
-    "An open source application built using the new router, server components and everything new in Next.js 13.",
+    "Cinevore, your social network for film discovery, built using the new router, server components and everything new in Next.js 13.",
   url: "https://cinevore.theodrosyimer.com",
   ogImage: "https://tx.shadcn.com/og.jpg",
   links: {
-    twitter: "https://twitter.com/",
-    github: "https://github.com/theodrosyimer/cinevore",
+    twitter: "https://twitter.com",
+    github: "https://github.com/theodrosyimer",
   },
 }
