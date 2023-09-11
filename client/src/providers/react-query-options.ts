d4eddd17f864fc301e4902efd52f4dc7f315f38b
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-09-11 12:29:27 +0200

    feat: add query options to react-query provider

---
+++ client/src/providers/react-query-options.ts
@@ -0,0 +1,12 @@
+export const queryClientOptions = {
+  defaultOptions: {
+    queries: {
+      refetchOnWindowFocus: false,
+      // retry: false,
+      // staleTime: 1000 * 60 * 5, // 5 minutes
+    },
+    // mutations: {
+    //   retry: false,
+    // },
+  },
+}
--- client/src/providers/react-query.tsx
+++ client/src/providers/react-query.tsx
@@ -1,10 +1,13 @@
 'use client'
 
+import { queryClientOptions } from '@/providers/react-query-options'
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
 import { useState } from 'react'
 
-export default function ReactQueryProvider({ children }: { children: React.ReactNode }) {
-  const [queryClient] = useState(() => new QueryClient())
+type Props = { children: React.ReactNode }
+
+export default function ReactQueryProvider({ children }: Props) {
+  const [queryClient] = useState(() => new QueryClient(queryClientOptions))
 
   return (
     <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
