ba614110ea24f2d82befb5b694a1df52e6d3e773
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-09-21 21:31:08 +0200

    refactor: refactor types for next-auth

--- client/src/types/next-auth.d.ts
+++ client/src/types/next-auth.d.ts
@@ -1,10 +1,16 @@
 import { NewUser } from "@/types/db"
+import { ValuesOf } from "@/types/utility"
+import { DefaultUser } from "next-auth"
 // import { User } from "next-auth"
 import { JWT } from "next-auth/jwt"
 
 type UserId = string
 
-type UserAuth = Pick<NewUser, 'id' | 'name' | 'email' | 'image' | 'role'>
+type Role = ValuesOf<Pick<NewUser, 'role'>>
+
+interface UserAuth extends DefaultUser {
+  role?: Role
+}
 
 declare module "next-auth/jwt" {
   interface JWT extends UserAuth {
