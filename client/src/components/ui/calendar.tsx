acbf88b4e81a1ce0e0d1ad8050dc7a2445066d79
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-09-23 02:50:24 +0200

    feat: add dropdown to slect month and year

--- client/src/components/ui/calendar.tsx
+++ client/src/components/ui/calendar.tsx
@@ -17,13 +17,16 @@ function Calendar({
 }: CalendarProps) {
   return (
     <DayPicker
+      // captionLayout="dropdown-buttons"
+      // fromYear={1920}
+      // toYear={new Date().getFullYear()}
       showOutsideDays={showOutsideDays}
       className={cn("p-3", className)}
       classNames={{
         months: "flex flex-col sm:flex-row space-y-4 sm:space-x-4 sm:space-y-0",
         month: "space-y-4",
         caption: "flex justify-center pt-1 relative items-center",
-        caption_label: "text-sm font-medium",
+        // caption_label: "text-sm font-medium",
         nav: "space-x-1 flex items-center",
         nav_button: cn(
           buttonVariants({ variant: "outline" }),
