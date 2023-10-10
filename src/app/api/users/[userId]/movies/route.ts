b316719fbb0d40c769fb180afa71ffbcbaac535b
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-10-10 18:10:01 +0200

    feat: add route to get all movies that a user watched or reviewed

---
+++ src/app/api/users/[userId]/movies/route.ts
@@ -0,0 +1,46 @@
+import { routeContextSchema } from "@/app/api/users/[userId]/route"
+import { isAdmin } from "@/lib/auth"
+import { db } from "@/lib/db"
+import { formatSimpleErrorMessage } from "@/lib/utils/utils"
+import { and, or, sql } from "drizzle-orm"
+import { getToken } from "next-auth/jwt"
+import { NextRequest } from "next/server"
+import { z } from "zod"
+
+export async function GET(
+  req: NextRequest,
+  context: z.infer<typeof routeContextSchema>
+) {
+  try {
+    const { params } = routeContextSchema.parse(context)
+
+    const token = await getToken({ req })
+
+    if ((token && params.userId === token.id) || isAdmin(token)) {
+
+      const moviesInfos = await db.query.movieInfosToUser.findMany({
+        where: (movieInfosToUser, { eq }) =>
+          and(
+            eq(movieInfosToUser.userId, params.userId),
+            or(eq(movieInfosToUser.watched, true),
+              eq(movieInfosToUser.reviewed, true)
+            )),
+      })
+
+      if (!moviesInfos) {
+        return new Response('Movies infos not found', { status: 404 })
+      }
+
+      return new Response(JSON.stringify(moviesInfos))
+    }
+
+    return new Response('Unauthorized', { status: 403 })
+  } catch (error) {
+    if (error instanceof Error) {
+      console.log(error)
+      return new Response(formatSimpleErrorMessage(error), { status: 500 })
+    }
+
+    return new Response(null, { status: 500 })
+  }
+}
