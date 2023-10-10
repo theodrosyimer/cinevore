ac0ffef78d051121d21fa6cfe8264bde2bc06a07
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-10-11 00:20:16 +0200

    fix: fix import errors

--- src/app/(site-layout)/(protected)/(me)/me/watchlist/page.tsx
+++ src/app/(site-layout)/(protected)/(me)/me/watchlist/page.tsx
@@ -1,20 +1,17 @@
-import FilmCardList from '@/components/film-card-list';
-import { authOptions } from '@/lib/auth';
-import { getCurrentUser } from '@/lib/session';
-import { redirect } from 'next/navigation';
+import { FilmCardList } from '@/components/film-card-list'
 
 export const metadata = {
   title: 'Diary Page',
   description: 'Your diary of films.',
-};
+}
 
 export default async function DiaryPage({
   params,
 }: {
   params: {
-    username: string;
-    id: number;
-  };
+    username: string
+    id: number
+  }
 }) {
   // const { user, isAdmin } = await getCurrentUser()
 
@@ -23,8 +20,8 @@ export default async function DiaryPage({
       <h1 className="text-4xl font-bold text-center">Watchlist</h1>
 
       <section>
-        <FilmCardList />
+        <FilmCardList aspectRatio='portrait' width='w92' />
       </section>
     </>
-  );
+  )
 }
