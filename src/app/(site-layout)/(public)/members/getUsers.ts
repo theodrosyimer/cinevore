ea0b5a5ecc921d510193540a1563edb826c5f49c
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2023-10-11 03:38:57 +0200

    fix: fix

--- src/app/(site-layout)/(public)/members/getUsers.ts
+++ src/app/(site-layout)/(public)/members/getUsers.ts
@@ -1,10 +1,10 @@
-import * as dotenv from 'dotenv';
-dotenv.config();
+import * as dotenv from 'dotenv'
+dotenv.config()
 
-import { userGETSchema } from '@/lib/validations/user';
-import { z } from 'zod';
+import { userGETSchema } from '@/lib/validations/user'
+import { z } from 'zod'
 
-const usersSchemas = z.array(userGETSchema);
+const usersSchemas = z.array(userGETSchema)
 
 export async function getUsers() {
   const data = await fetch(`${process.env.SERVER_URL}/api/users`, {
@@ -13,14 +13,14 @@ export async function getUsers() {
       'Content-Type': 'application/json',
     },
     // cache: "no-cache",
-  });
-  const json = (await data.json()) as unknown;
-  const validatedUsers = usersSchemas.safeParse(json);
+  })
+  const json = await data.json() as unknown
+  const validatedUsers = usersSchemas.safeParse(json)
 
   if (!validatedUsers.success) {
-    console.error(validatedUsers.error.message);
-    return [];
+    console.error(validatedUsers.error.message)
+    return []
   }
 
-  return validatedUsers.data;
+  return validatedUsers.data
 }
