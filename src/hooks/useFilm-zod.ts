935e485bff6a299fb758707c400baafad54457f6
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-29 08:36:49 +0100

    refactor: refactor film-related hooks to use signal for cancellation

--- src/hooks/useFilm-zod.ts
+++ src/hooks/useFilm-zod.ts
@@ -1,15 +1,10 @@
 import { searchByID } from '@/lib/tmdb/src/tmdb'
 import { useQuery } from '@tanstack/react-query'
 
-async function getFilmById(id: string) {
-  const data = await searchByID({ id, category: 'movie' })
-  return data
-}
-
 export function useFilm(id: string) {
   const filmData = useQuery({
     queryKey: ['film', id],
-    queryFn: () => getFilmById(id),
+    queryFn: ( {signal}) => searchByID({ id, category: 'movie' }, { signal }),
   })
   return filmData
 }
