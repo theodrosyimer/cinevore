5089b06307b34514c41345e26199fb0f53f4b63a
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-01-31 14:22:12 +0100

    feat: add font size options to tailwind.config.cjs

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -82,6 +82,17 @@ module.exports = {
         'accordion-down': 'accordion-down 0.2s ease-out',
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
+      fontSize: {
+        sm: 'clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem)',
+        base: 'clamp(1rem, 0.34vw + 0.91rem, 1.19rem)',
+        lg: 'clamp(1.25rem, 0.61vw + 1.1rem, 1.58rem)',
+        xl: 'clamp(1.56rem, 1vw + 1.31rem, 2.11rem)',
+        '2xl': 'clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)',
+        '3xl': 'clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)',
+        '4xl': 'clamp(3.05rem, 3.54vw + 2.17rem, 5rem)',
+        '5xl': 'clamp(3.81rem, 5.18vw + 2.52rem, 6.66rem)',
+        '6xl': 'clamp(4.77rem, 7.48vw + 2.9rem, 8.88rem)',
+      },
       gridTemplateRows: {
         // Complex site-specific row configuration
         layout: 'auto 1fr auto',
