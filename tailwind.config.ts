11ea7c285cc227f4ae82c21100b5f8a7271d5309
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-18 16:17:30 +0100

    config: update tailwind.config.ts file

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,9 +1,8 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
+import { type Config } from "tailwindcss";
+import { fontFamily } from "tailwindcss/defaultTheme";
+export default {
   darkMode: ['class'],
   content: [
-    './components/**/*.{ts,tsx}',
-    // './app/**/*.{ts,tsx}',
     './src/**/*.{ts,tsx}',
   ],
   theme: {
@@ -70,12 +69,12 @@ module.exports = {
       },
       keyframes: {
         'accordion-down': {
-          from: { height: 0 },
+          from: { height: '0' },
           to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
           from: { height: 'var(--radix-accordion-content-height)' },
-          to: { height: 0 },
+          to: { height: '0' },
         },
       },
       animation: {
@@ -132,4 +131,4 @@ module.exports = {
     },
   },
   plugins: [require('tailwindcss-animate')],
-}
+} satisfies Config
