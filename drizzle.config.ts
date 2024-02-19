0894640429c2a4338ceda2d36c558bd35a7393cd
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-19 21:07:41 +0100

    fix: fix drizzle.config.ts with environment variable for database connection string

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,10 +1,12 @@
 import type { Config } from "drizzle-kit"
+import { env } from "@/env.js"
+
 export default {
   schema: "./src/db/schema/planetscale/index.ts",
   out: "./drizzle",
   driver: 'mysql2',
   dbCredentials: {
-    connectionString: process.env.DATABASE_URL ?? '',
+    connectionString: env.DATABASE_URL,
     // host: env.DATABASE_HOST ?? '',
     // user: env.DATABASE_USERNAME ?? '',
     // database: env.DATABASE_NAME ?? '',
