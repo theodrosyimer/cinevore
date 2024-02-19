f5ba8eaf56dadf216ff84262be8e17d44aacc3cb
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-19 12:59:04 +0100

    fix: fix import statement and update Twitter link in site configuration

--- src/config/site.ts
+++ src/config/site.ts
@@ -1,13 +1,13 @@
-import { SiteConfig } from '@/types'
+import { type SiteConfig } from '@/types'
 
 export const siteConfig: SiteConfig = {
   name: 'Cinevore',
   description:
     'Cinevore, your social network for film discovery, built using the new router, server components and everything new in Next.js 13.',
   url: 'https://cinevore.theodrosyimer.com',
-  ogImage: 'https://tx.shadcn.com/og.jpg',
+  ogImage: 'https://cinevore.theodrosyimer.com/og.jpg',
   links: {
-    twitter: 'https://twitter.com',
+    twitter: 'https://twitter.com/TH2theEO',
     github: 'https://github.com/theodrosyimer/cinevore',
   },
 }
