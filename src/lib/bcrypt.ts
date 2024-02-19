89f130fce3b17e95df7d17dec4cdd33f01018b61
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-19 22:03:31 +0100

    feat: add @node-rs/bcrypt for password hashing and validation

--- src/lib/bcrypt.ts
+++ src/lib/bcrypt.ts
@@ -1,13 +1,42 @@
-import bcrypt from 'bcrypt'
+// import bcrypt from 'bcrypt'
+
+import { hash, compare } from "@node-rs/bcrypt";
 
 const SALT_ROUNDS = 10
+
+// export async function validateUserPassword(
+//   password: string | Buffer,
+//   hash: string,
+// ) {
+//   return bcrypt
+//     .compare(password, hash)
+//     .then((isValidPassword) => isValidPassword)
+//     .catch((err) => {
+//       if (err instanceof Error) {
+//         console.error(err.message)
+//       }
+//     })
+// }
+
+// export async function hashPassword(
+//   password: string | Buffer,
+//   saltRounds: string | number = SALT_ROUNDS,
+// ) {
+//   return bcrypt
+//     .hash(password, saltRounds)
+//     .then((hash) => hash)
+//     .catch((err) => {
+//       if (err instanceof Error) {
+//         console.error(err.message)
+//       }
+//     })
+// }
 
 export async function validateUserPassword(
   password: string | Buffer,
   hash: string,
 ) {
-  return bcrypt
-    .compare(password, hash)
+  return compare(password, hash)
     .then((isValidPassword) => isValidPassword)
     .catch((err) => {
       if (err instanceof Error) {
@@ -18,10 +47,11 @@ export async function validateUserPassword(
 
 export async function hashPassword(
   password: string | Buffer,
-  saltRounds: string | number = SALT_ROUNDS,
+  cost: string | number,
 ) {
-  return bcrypt
-    .hash(password, saltRounds)
+  cost ??= SALT_ROUNDS
+
+  return hash(password, cost)
     .then((hash) => hash)
     .catch((err) => {
       if (err instanceof Error) {
