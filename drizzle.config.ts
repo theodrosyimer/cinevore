b2d8f509f706eb2a597572bf4daa9070f27f94d3
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-21 13:51:51 +0100

    fix: update drizzle.config.ts file

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -1,16 +1,16 @@
-import type { Config } from "drizzle-kit"
-import { env } from "@/env.js"
+import type { Config } from 'drizzle-kit'
+import { env } from '@/env.js'
 
 export default {
-  schema: "./src/db/schema/planetscale/index.ts",
-  out: "./drizzle",
+  schema: './src/db/schema/planetscale/index.ts',
+  out: './drizzle',
   driver: 'mysql2',
   dbCredentials: {
-    connectionString: env.DATABASE_URL,
+    uri: env.DATABASE_URL,
     // host: env.DATABASE_HOST ?? '',
     // user: env.DATABASE_USERNAME ?? '',
     // database: env.DATABASE_NAME ?? '',
     // password: env.DATABASE_PASSWORD,
     // port: Number(env.DB_PORT),
-  }
+  },
 } satisfies Config
