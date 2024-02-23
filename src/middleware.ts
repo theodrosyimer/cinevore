fb3be65312ce81b266bc2375eae346063122ada2
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-24 00:46:45 +0100

    refactor: refactor middleware.ts

--- src/middleware.ts
+++ src/middleware.ts
@@ -6,12 +6,32 @@ export default withAuth(
   async function middleware(req) {
     const token = await getToken({ req })
     const isAuthorized = !!token
+
     const isAdminSegment = req.nextUrl.pathname.startsWith('/admin')
     const isMeSegment = req.nextUrl.pathname.startsWith('/me')
     const isSettingsSegment = req.nextUrl.pathname.startsWith('/settings')
+
     const isAuthPage =
       req.nextUrl.pathname.startsWith('/login') ||
       req.nextUrl.pathname.startsWith('/register')
+
+    const hasNoAccessToUserProtectedRoutes =
+      (isMeSegment || isSettingsSegment) && !isAuthorized
+
+    const hasNoAccessToAdminProtectedRoutes =
+      isAdminSegment &&
+      !(token?.role === 'admin' || token?.role === 'superadmin')
+
+    if (isAuthPage) {
+      if (isAuthorized) {
+        return NextResponse.redirect(new URL('/me', req.url))
+      }
+      return null
+    }
+
+    if (hasNoAccessToAdminProtectedRoutes || hasNoAccessToUserProtectedRoutes) {
+      return NextResponse.redirect(new URL('/', req.url))
+    }
 
     if (!isAuthorized) {
       let from = req.nextUrl.pathname
@@ -23,17 +43,6 @@ export default withAuth(
       return NextResponse.redirect(
         new URL(`/login?from=${encodeURIComponent(from)}`, req.url),
       )
-    }
-
-    if (isAuthPage && isAuthorized) {
-      return NextResponse.redirect(new URL('/me', req.url))
-    }
-
-    if (
-      (isAdminSegment || isMeSegment || isSettingsSegment) &&
-      !(token?.role === 'admin' || token?.role === 'superadmin')
-    ) {
-      return NextResponse.redirect(new URL('/', req.url))
     }
 
     return null
