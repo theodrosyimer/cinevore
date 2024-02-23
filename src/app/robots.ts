0e1b534f43bdd4ad4c54851ff6850e291c6e6598
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-23 03:47:43 +0100

    feat: update robots.txt rules

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -4,7 +4,10 @@ export default function robots(): MetadataRoute.Robots {
   return {
     rules: {
       userAgent: '*',
-      allow: '/',
+      allow: [
+        '/',
+        /*  '/about', '/members', '/films', '/reviews',  */ '/api/og/*',
+      ],
     },
   }
 }
