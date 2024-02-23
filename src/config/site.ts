c2dde50c6a8c199357ad57945a01e7facdd0b8ea
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-23 04:02:05 +0100

    fix: update site URL and ogImage URL in siteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,10 +4,11 @@ export const siteConfig: SiteConfig = {
   name: 'Cinevore',
   description:
     'Cinevore, your social network for film discovery, built using the new router, server components and everything new in Next.js 14.',
-  // url: 'https://cinevore.theodrosyimer.com',
-  url: 'https://tx.shadcn.com',
-  // ogImage: 'https://cinevore.theodrosyimer.com/og.jpg',
-  ogImage: 'https://tx.shadcn.com/og.jpg',
+  url: 'https://vercel.live/link/cinevore-git-dev-theodrosyimer.vercel.app?via=deployment-domains-list-branch',
+  // url: 'https://tx.shadcn.com',
+  ogImage:
+    'https://vercel.live/link/cinevore-git-dev-theodrosyimer.vercel.app?via=deployment-domains-list-branch/og.jpg',
+  // ogImage: 'https://tx.shadcn.com/og.jpg',
   links: {
     twitter: 'https://twitter.com/TH2theEO',
     github: 'https://github.com/theodrosyimer/cinevore',
