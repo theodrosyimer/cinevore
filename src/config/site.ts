3c24dcf78dcf27148913751632b3ea184002b13f
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-25 00:32:24 +0100

    refactor: update site URL in siteConfig

--- src/config/site.ts
+++ src/config/site.ts
@@ -4,8 +4,8 @@ export const siteConfig: SiteConfig = {
   name: 'Cinevore',
   description:
     'Cinevore, your social network for film discovery, built using the new router, server components and everything new in Next.js 14.',
-  // url: 'https://cinevore-git-dev-theodrosyimer.vercel.app',
-  url: 'https://tx.shadcn.com',
+  url: 'https://cinevore-git-dev-theodrosyimer.vercel.app',
+  // url: 'https://tx.shadcn.com',
   // ogImage: 'https://cinevore-git-dev-theodrosyimer.vercel.app/og.jpg',
   ogImage: 'https://tx.shadcn.com/og.jpg',
   links: {
