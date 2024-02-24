ca5a2b749b8b3dc42e30507a3422c751c1a22096
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-02-24 02:56:46 +0100

    fix: update tailwind.config.ts for variable font sizes

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,10 +1,8 @@
-import { type Config } from "tailwindcss";
-import { fontFamily } from "tailwindcss/defaultTheme";
+import { type Config } from 'tailwindcss'
+import { fontFamily } from 'tailwindcss/defaultTheme'
 export default {
   darkMode: ['class'],
-  content: [
-    './src/**/*.{ts,tsx}',
-  ],
+  content: ['./src/**/*.{ts,tsx}'],
   theme: {
     container: {
       center: true,
@@ -82,18 +80,17 @@ export default {
         'accordion-up': 'accordion-up 0.2s ease-out',
       },
       fontSize: {
-        sm: 'clamp(0.8rem, 0.17vw + 0.76rem, 0.89rem)',
-        base: 'clamp(1rem, 0.34vw + 0.91rem, 1.19rem)',
-        lg: 'clamp(1.25rem, 0.61vw + 1.1rem, 1.58rem)',
-        xl: 'clamp(1.56rem, 1vw + 1.31rem, 2.11rem)',
-        '2xl': 'clamp(1.95rem, 1.56vw + 1.56rem, 2.81rem)',
-        '3xl': 'clamp(2.44rem, 2.38vw + 1.85rem, 3.75rem)',
-        '4xl': 'clamp(3.05rem, 3.54vw + 2.17rem, 5rem)',
-        '5xl': 'clamp(3.81rem, 5.18vw + 2.52rem, 6.66rem)',
-        '6xl': 'clamp(4.77rem, 7.48vw + 2.9rem, 8.88rem)',
+        sm: 'clamp(0.8rem, -0.08vw + 0.82rem, 0.75rem)',
+        base: 'clamp(1rem, 0vw + 1rem, 1rem)',
+        lg: 'clamp(1.25rem, 0.14vw + 1.22rem, 1.33rem)',
+        xl: 'clamp(1.56rem, 0.36vw + 1.49rem, 1.78rem)',
+        '2xl': 'clamp(1.95rem, 0.69vw + 1.81rem, 2.37rem)',
+        '3xl': 'clamp(2.44rem, 1.19vw + 2.2rem, 3.16rem)',
+        '4xl': 'clamp(3.05rem, 1.93vw + 2.67rem, 4.21rem)',
+        '5xl': 'clamp(3.81rem, 2.99vw + 3.22rem, 5.61rem)',
+        '6xl': 'clamp(4.77rem, 4.52vw + 3.87rem, 7.48rem)',
       },
       gridTemplateRows: {
-        // Complex site-specific row configuration
         layout: 'auto 1fr auto',
       },
       minHeight: {
