5bf9b6d679b56bb643949e0b98fd0bb2a7a19576
Theodros Yimer <12742021+theodrosyimer@users.noreply.github.com>
2024-03-01 05:06:58 +0100

    refactor: refactor SiteLayout component name to Layout

--- src/app/(site-layout)/layout.tsx
+++ src/app/(site-layout)/layout.tsx
@@ -1,11 +1,11 @@
 import { SiteFooter } from './_layout/footer'
 import { SiteHeader } from './_layout/header'
 
-interface SiteLayoutProps {
+interface LayoutProps {
   children: React.ReactNode
 }
 
-export default async function SiteLayout({ children }: SiteLayoutProps) {
+export default async function Layout({ children }: LayoutProps) {
   return (
     <>
       <SiteHeader />
